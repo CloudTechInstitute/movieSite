6ac5494cd94077ff773de0f19fe643b1c549e496
EmmanuelCBS <emmanuel.abobila@casanteysmartsuite.com>
2025-01-21 17:53:50 +0000

    first commit

---
+++ src/index.php
@@ -0,0 +1,135 @@
+<?php include 'header.php'; ?>
+
+<body class="relative flex flex-col justify-center px-64 py-6 bg-gray-900 bg-cover bg-center bg-fixed"
+    style="background-image: url('images/13124.jpg');">
+    <!-- Blurred Background -->
+    <div class="absolute inset-0 bg-gray-900 bg-opacity-50 filter blur-sm"></div>
+
+    <!-- Content Layer -->
+    <div class="relative z-10">
+        <div class="w-full flex items-center justify-between mb-10 text-white">
+            <h1 class="text-center">Home</h1>
+            <h1 class="text-center">Movies</h1>
+            <h1 class="text-center">Series</h1>
+            <h1 class="text-center">Profile</h1>
+            <h1 class="text-center">Settings</h1>
+            <input type="text" class="outline outline-1 outline-gray-400 bg-transparent p-2 rounded-md"
+                placeholder="search" />
+        </div>
+        <div class="flex items-start justify-center gap-10 mb-10 px-36">
+            <div class="p-2 [w-50%]">
+                <p class="text-4xl font-bold text-white mb-4">
+                    Moonlight
+                </p>
+                <p class=" text-white text-sm mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a type </p>
+                <p class=" text-yellow-300">4-star rating </p>
+                <p class="text-2xl  text-yellow-300 mb-4">1h 45m </p>
+
+                <div class="flex justify-start gap-4">
+                    <button class="bg-blue-300 p-2 w-full rounded-sm ">Play</button>
+                    <button class="bg-blue-300 p-2 w-full rounded-sm ">Download</button>
+                </div>
+            </div>
+            <div class="p-2 [w-50%]">
+                <div class="outline outline-1 outline-blue-300 h-72 w-52 overflow-hidden shadow-blue-300 shadow-lg">
+                    <img src="images/moon.jpg" alt="moonlight poster" class="w-full h-full object-cover" />
+                </div>
+            </div>
+        </div>
+        <div class="w-full">
+            <div class="grid grid-cols-5 gap-6 w-full text-white">
+                <div class=" w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/black.jpg" alt="black panther poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/star.jpg" alt="star wars poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/bat.jpg" alt="bat man poster" class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class=" w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/black.jpg" alt="black panther poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/star.jpg" alt="star wars poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/bat.jpg" alt="bat man poster" class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class=" w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/black.jpg" alt="black panther poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/star.jpg" alt="star wars poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/bat.jpg" alt="bat man poster" class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class=" w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/black.jpg" alt="black panther poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/star.jpg" alt="star wars poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/bat.jpg" alt="bat man poster" class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class=" w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/black.jpg" alt="black panther poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/star.jpg" alt="star wars poster"
+                            class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="w-full outline outline-1 outline-gray-500 h-64 rounded-md overflow-hidden">
+                        <img src="images/bat.jpg" alt="bat man poster" class="w-full h-full object-cover rounded-md" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
